c3825dd1eb598173f9fafc56e540896ef7649053
ninomae <orangerange-s.n@hotmail.co.jp>
2025-01-17 03:00:45 +0900

    小文字にする

--- src/vector/create-collection.ts
+++ src/vector/create-collection.ts
@@ -2,6 +2,6 @@ import { QdrantClient } from "@qdrant/js-client-rest";
 
 const qdrantClient = new QdrantClient();
 
-await qdrantClient.createCollection("RAG_HANDS_ON", {
+await qdrantClient.createCollection("rag_hands_on", {
 	vectors: { size: 512, distance: "Cosine" },
 });
