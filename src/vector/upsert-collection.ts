7f9f788656a69b1d6241fd37adad29acf60773c3
ninomae <orangerange-s.n@hotmail.co.jp>
2025-01-17 03:19:51 +0900

    upsert-collection

--- src/vector/upsert-collection.ts
+++ src/vector/upsert-collection.ts
@@ -1,7 +1,9 @@
 import * as fs from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import { openai } from "@ai-sdk/openai";
 import { QdrantClient } from "@qdrant/js-client-rest";
+const model = openai.embedding("text-embedding-3-small", { dimensions: 512 });
 const qdrantClient = new QdrantClient();
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,4 +12,21 @@ const __dirname = path.dirname(__filename);
 const data = await fs.readFile(path.resolve(__dirname, "./data.txt"), {
 	encoding: "utf8",
 });
-console.log(data);
+
+const { embeddings } = await model.doEmbed({
+	values: [data],
+});
+
+const points = embeddings.map((embedding, index) => {
+	return {
+		id: index,
+		payload: {
+			value: data[index],
+		},
+		vector: embedding,
+	};
+});
+
+await qdrantClient.upsert("rag_hands_on", {
+	points,
+});
